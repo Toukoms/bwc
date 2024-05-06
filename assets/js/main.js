094bbd005a76c334c0d905e01ceee6b500844be0
Toukom's <tokiniaina090605@gmail.com>
2024-05-06 20:11:22 +0300

    feat: add contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,53 @@ function redirectTo(path) {
   window.location.href = "http://localhost:5500" + path;
 }
 
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  const about = document.getElementsByClassName("about")[0];
-  const contact = document.getElementsByClassName("contact")[0];
-  const hero = document.getElementsByClassName("hero")[0];
+// Animation loading when page on load
+window.addEventListener("load", () => {
+  const loading = document.getElementsByClassName("loading")[0];
+  const html = document.getElementsByTagName("html")[0];
+  loading.classList.remove("active");
+  html.style.overflowY = "scroll";
+});
 
-  console.log(window.location.href);
-  console.log(640 < window.scrollY < 1280);
+// window.addEventListener("scroll", () => {
+//   const about = document.getElementsByClassName("about")[0];
+//   const contact = document.getElementsByClassName("contact")[0];
+//   const hero = document.getElementsByClassName("hero")[0];
 
-  if (640 < window.scrollY && window.scrollY < 1280) {
-    hero.classList.remove("active");
-    about.classList.add("active");
-    contact.classList.remove("active");
-  } else if (window.scrollY > 1280) {
-    hero.classList.remove("active");
-    about.classList.remove("active");
-    contact.classList.add("active");
-  } else {
-    hero.classList.add("active");
-    about.classList.remove("active");
-    contact.classList.remove("active");
-  }
-})
+//   if (640 < window.scrollY && window.scrollY < 1280) {
+//     hero.classList.remove("active");
+//     about.classList.add("active");
+//     contact.classList.remove("active");
+//   } else if (window.scrollY > 1280) {
+//     hero.classList.remove("active");
+//     about.classList.remove("active");
+//     contact.classList.add("active");
+//   } else {
+//     hero.classList.add("active");
+//     about.classList.remove("active");
+//     contact.classList.remove("active");
+//   }
+// })
+
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll("header nav a");
+
+console.log(sections);
+
+const observer = new IntersectionObserver((entries) => {
+  console.log(entries);
+  entries.forEach((entry, i) => {
+    if (entry.intersectionRatio > 0.9) {
+      links.forEach((link) => {
+        link.classList.remove("active");
+        if (link.classList.contains(entry.target.id)) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
